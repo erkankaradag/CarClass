d91399a6ddfac095d382ea6dd5108bceee6893be
erkankaradag <erkankaradagcom@gmail.com>
2025-01-10 01:24:14 +0300

    Add project files.

---
+++ CarClass/Car.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarClass
+{
+    public class Car
+    {
+        private string brand;
+        private string model;
+        private string color;
+        private int numberOfDoors;
+
+
+        public string Brand
+        {
+            get { return brand; }
+            set { brand = value; }
+        }
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+        public string Color
+        {
+            get { return color; }
+            set { color = value; }
+        }
+        public int NumberOfDoors
+        {
+            get { return numberOfDoors; }
+            set
+            {
+                if ( value == 2 || value == 4 )
+                {
+                    numberOfDoors = value;
+                }
+                else
+                {
+                    Console.WriteLine("Geçersiz kapı sayısı! Kapı sayısı yalnızca 2 veya 4 olabilir.");
+                    numberOfDoors = -1;
+                }
+
+            }
+        }
+
+        public void CarDetails()
+        {
+            Console.WriteLine($"Marka: {Brand}");
+            Console.WriteLine($"Model: {Model}");
+            Console.WriteLine($"Renk: {Color}");
+            Console.WriteLine($"KapıSayısı: {NumberOfDoors}");
+
+        }
+
+
+    }
+
+}
---
+++ CarClass/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace CarClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car
+            {
+                Brand = "Volkswagen",
+                Model = "Caddy",
+                Color = "Azul Mavisi",
+                NumberOfDoors = 4
+            };
+            car1.CarDetails();
+
+            Car car2 = new Car
+            {
+                Brand = "Volkswagen",
+                Model = "Tiguan",
+                Color = "Siyah",
+                NumberOfDoors = 3
+            };
+            car2.CarDetails();
+
+        }
+    }
+}
